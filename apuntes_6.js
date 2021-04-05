bb8c73299a312e58ff6d8226e8601cf5084d9d50
a308728 <abertotto@santandertecnologia.com.ar>
2021-04-05 17:46:47 -0300

    memorias

---
+++ apuntes_6.js
@@ -0,0 +1,18 @@
+//La memoria principal posee las siguientes características:
+//Opción 2: Su capacidad es limitada.
+//Opción 4: Su velocidad es mayor que la memoria secundaria.
+//Opción 5: Su costo es más elevado que la memoria secundaria.
+//Opción 1: Puede ser volátil.
+//Para acceder simultáneamente a dos módulos de memoria RAM, estos deben ser: Opción 1: De la misma capacidad, velocidad, frecuencia, latencia y fabricante.
+//¿Qué es la latencia?. Opción 4: Es la cantidad de ciclos de reloj que transcurren entre una petición y su respuesta.
+//Qué es la memoria principal? Opción 2: Es la memoria de la computadora donde se almacenan temporalmente tanto los datos como los programas que la CPU está procesando, o va a procesar, en un determinado momento.
+//La memoria RAM...Opción 1: Significa memoria de acceso aleatorio.
+//La memoria caché es.. Opción 3: Un componente que guarda datos para que las solicitudes futuras de esos datos se puedan atender con mayor rapidez.
+//La memoria caché del procesador contiene los niveles L1, L2, L3 y L4.
+//Opción 2: L1 es el nivel con menor capacidad.
+//Opción 4: Cada nivel puede almacenar la misma información que se encuentra en el nivel anterior.
+//Cuando el CPU necesita un dato, ¿cómo lo busca? Opción 2: Lo busca en su memoria caché y si no lo encuentra, se lo pide a la memoria RAM.
+//los discos solidos NO funcionan con magnetismo.
+//¿Cuál es la menor unidad de medida de almacenamiento?Opción 2: Un bit.
+//Enunciado: Un bit es... Opción 3: Un dígito del sistema binario que puede ser 0 o 1.
+//Un byte es... Opción 2: 8 bits.
