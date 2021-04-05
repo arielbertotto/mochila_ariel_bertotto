47e57352fdb40290de542f155b98690eb4cb912a
a308728 <abertotto@santandertecnologia.com.ar>
2021-04-05 17:39:17 -0300

    github

---
+++ apuntes_4.js
@@ -0,0 +1,3 @@
+//Si bien tienen un nombre similar, Git es la tecnología que mantiene el sistema de versionado, mientras que GitHub es un proveedor del servicio. Hay muchos más, como GitLab o Bitbucket, los invitamos a visitar sus páginas webs y ver qué ofrecen.
+//Recordá que es necesario crear un vínculo entre ambos para poder mantener actualizados los archivos locales que están conectados a ese repositorio en la nube.
+//Es una muy buena práctica crear una rama (branch) para trabajar en una nueva funcionalidad así no afectamos el proyecto hasta que esta funcionalidad esté finalizada.
