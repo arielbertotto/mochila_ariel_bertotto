7dab11879b7a2d34501035ca8c8d7e3da6d2c39a
a308728 <abertotto@santandertecnologia.com.ar>
2021-04-05 17:41:28 -0300

    estructura

---
+++ apuntes_5.js
@@ -0,0 +1,6 @@
+//una computadora Es esencialmente una máquina que recibe datos, los procesa y muestra los resultados. Los mismos pueden ser almacenados, transmitidos o impresos .
+//los componentes internos de una computadora Son todos los elementos físicos inseparables de la computadora. Si faltara alguno de ellos, la misma podría funcionar de manera incorrecta o no hacerlo.
+//los perifericos Son dispositivos que pueden añadir funciones u operaciones, pero no son parte esencial de la misma.
+//CPU Es el encargado de procesar todas las tareas de nuestra PC, a través de la resolución de instrucciones lógicas y matemáticas que están almacenadas en la memoria RAM.
+//GPU graphics processing unit, gráficas, pintar pixeles del monitor
+//cpu es de uso general, y gpu es de uso específico
