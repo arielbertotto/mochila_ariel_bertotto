bb072ee0feec2bbd21717724bdd414fa1e8d9fe6
a308728 <abertotto@santandertecnologia.com.ar>
2021-04-05 19:52:19 -0300

    sistemas operativos

---
+++ apuntes_7.js
@@ -0,0 +1,6 @@
+//Sistema Operativo	Windows 10	Mac Os X	GNU/Linux	Android	iOS	Chrome OS
+//Según Usuario	Multiusuario	Multiusuario	Multiusuario	Multiusuario	Multiusuario	Multiusuario
+//Según Gestión de Tareas	Multitareas	Multitareas	Multitareas	Multitareas	Multitareas	Multitareas
+//Según Gestión de Recursos	Centralizado	Centralizado	Centralizado	Centralizado	Centralizado	Centralizado
+//Según Estructura Interna	Híbrido (tiende a monolítico)	Monolítico	Monolítico (puede ser jerarquíco)	Monolítico	Monolítico	Monolíticos
+//Dispositivos que lo usan	PC, Notebooks	PC, Notebooks	De todo	Celulares, tablets	Celulares, tablets	PC, Notebooks
